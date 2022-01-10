0282ea39b79f73d756e5635a8463d27bc101e70c
SAWADA Tadashi <cesare@mayverse.jp>
2022-01-10 19:32:07 +0900

    Add database connection

---
+++ backend/graphqlexp-adapter/src/persistence/mod.rs
@@ -0,0 +1,24 @@
+use std::env;
+use std::sync::Arc;
+
+use anyhow::Result;
+use sqlx::postgres::{PgPool, PgPoolOptions};
+
+pub struct Database {
+    #[allow(dead_code)]
+    pub(crate) pool: Arc<PgPool>,
+}
+
+impl Database {
+    pub async fn create() -> Result<Self> {
+        let url = env::var("DATABASE_URL")?;
+        let pool = PgPoolOptions::new()
+            .connect(&url)
+            .await?;
+
+        let db = Self {
+            pool: Arc::new(pool),
+        };
+        Ok(db)
+    }
+}
