662788117bc3e948fd86d6c25e5c2b518fbb221e
SAWADA Tadashi <cesare@mayverse.jp>
2022-01-11 23:25:32 +0900

    Enable to find servant

--- backend/graphqlexp-adapter/src/repositories/servant.rs
+++ backend/graphqlexp-adapter/src/repositories/servant.rs
@@ -1,16 +1,31 @@
 use anyhow::Result;
 use async_trait::async_trait;
+use sqlx::query_as;
 
 use graphqlexp_kernel::{
     models::servant::{Servant, ServantId},
     repositories::servant::ServantRepository,
 };
 use super::Repository;
+use crate::records::servant::ServantRecord;
 
 #[async_trait]
 impl ServantRepository for Repository<Servant> {
-    #[allow(unused_variables)]
     async fn find(&self, id: ServantId) -> Result<Option<Servant>> {
-        todo!()
+        let pool = self.database.pool.clone();
+        let statement = "
+            select id, name, class_name, rarity, created_at, updated_at
+            from servants
+            where id = ?
+            limit 1
+        ";
+        let result = query_as::<_, ServantRecord>(statement)
+            .bind(id.value)
+            .fetch_optional(&*pool)
+            .await?;
+        match result {
+            Some(record) => Ok(Some(record.try_into()?)),
+            None => Ok(None),
+        }
     }
 }
