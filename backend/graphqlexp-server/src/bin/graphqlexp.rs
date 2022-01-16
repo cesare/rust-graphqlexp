e1b27155df5632423e66e49d434d4b2ae3e782fa
SAWADA Tadashi <cesare@mayverse.jp>
2022-01-16 20:15:42 +0900

    Setup logger

--- backend/graphqlexp-server/src/bin/graphqlexp.rs
+++ backend/graphqlexp-server/src/bin/graphqlexp.rs
@@ -1,9 +1,19 @@
-use actix_web::{App, HttpServer};
+use actix_web::{App, HttpServer, middleware::Logger};
+use anyhow::Result;
+use simplelog::{Config, LevelFilter, SimpleLogger};
+
+fn initialize_logger() -> Result<()> {
+    SimpleLogger::init(LevelFilter::Info, Config::default())?;
+    Ok(())
+}
 
 #[actix_rt::main]
-async fn main() -> anyhow::Result<()> {
+async fn main() -> Result<()> {
+    initialize_logger()?;
+
     let server = HttpServer::new(move || {
         App::new()
+            .wrap(Logger::new("%a %t \"%r\" %s %b \"%{Referer}i\" \"%{User-Agent}i\" %T"))
     });
     server.bind("127.0.0.1:8080")?.run().await?;
 
