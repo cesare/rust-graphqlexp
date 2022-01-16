e959f1a603682547e3df68b7b5b84822959ad377
SAWADA Tadashi <cesare@mayverse.jp>
2022-01-16 19:58:29 +0900

    Enable to start up http server

--- backend/graphqlexp-server/src/bin/graphqlexp.rs
+++ backend/graphqlexp-server/src/bin/graphqlexp.rs
@@ -1,2 +1,11 @@
-fn main() {
+use actix_web::{App, HttpServer};
+
+#[actix_rt::main]
+async fn main() -> anyhow::Result<()> {
+    let server = HttpServer::new(move || {
+        App::new()
+    });
+    server.bind("127.0.0.1:8080")?.run().await?;
+
+    Ok(())
 }
