cf14ca152f24273a377fa6fcc8679a8c4c3077c4
SAWADA Tadashi <cesare@mayverse.jp>
2022-01-25 23:15:12 +0900

    Refactor handlers

--- backend/graphqlexp-server/src/routes.rs
+++ backend/graphqlexp-server/src/routes.rs
@@ -1,15 +1,23 @@
-use actix_web::{web, Error, HttpResponse};
+use actix_web::{
+    Error, HttpResponse,
+    web::{
+        Data, Json, ServiceConfig,
+        block, post, resource,
+    }
+};
 use anyhow::Result;
 use juniper::http::GraphQLRequest;
 
 use crate::schema::Schema;
 
-pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::resource("/graphql").route(web::post().to(graphql)));
+pub fn configure_routes(cfg: &mut ServiceConfig) {
+    cfg.service(
+        resource("/graphql").route(post().to(graphql))
+    );
 }
 
-async fn graphql(st: web::Data<Schema>, data: web::Json<GraphQLRequest>) -> Result<HttpResponse, Error> {
-    let servant = web::block(move || {
+async fn graphql(st: Data<Schema>, data: Json<GraphQLRequest>) -> Result<HttpResponse, Error> {
+    let servant = block(move || {
         let res = data.execute_sync(&st, &());
         serde_json::to_string(&res)
     }).await??;
