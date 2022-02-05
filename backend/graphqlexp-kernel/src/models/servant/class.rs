34dfe42eab83169198155a42321cda5ab63ae4c0
SAWADA Tadashi <cesare@mayverse.jp>
2022-02-05 22:14:38 +0900

    Move class to submodule

---
+++ backend/graphqlexp-kernel/src/models/servant/class.rs
@@ -0,0 +1,64 @@
+use std::str::FromStr;
+use std::string::ToString;
+
+use anyhow::anyhow;
+
+pub enum Class {
+    Saber,
+    Archer,
+    Lancer,
+    Rider,
+    Caster,
+    Assassin,
+    Berserker,
+    Ruler,
+    Avenger,
+    Mooncancer,
+    Alterego,
+    Pretender,
+    Foreigner,
+}
+
+impl FromStr for Class {
+    type Err = anyhow::Error;
+
+    fn from_str(class_name: &str) -> Result<Self, Self::Err> {
+        match class_name {
+            "saber" => Ok(Self::Saber),
+            "archer" => Ok(Self::Archer),
+            "lancer" => Ok(Self::Lancer),
+            "rider" => Ok(Self::Rider),
+            "caster" => Ok(Self::Caster),
+            "assassin" => Ok(Self::Assassin),
+            "berserker" => Ok(Self::Berserker),
+            "ruler" => Ok(Self::Ruler),
+            "avenger" => Ok(Self::Avenger),
+            "mooncancer" => Ok(Self::Mooncancer),
+            "alterego" => Ok(Self::Alterego),
+            "pretender" => Ok(Self::Pretender),
+            "foreigner" => Ok(Self::Foreigner),
+            _ => Err(anyhow!("Unknown servant class name: {}", class_name)),
+        }
+    }
+}
+
+impl ToString for Class {
+    fn to_string(&self) -> String {
+        let class_name = match self {
+            Self::Saber => "saber",
+            Self::Archer => "archer",
+            Self::Lancer => "lancer",
+            Self::Rider => "rider",
+            Self::Caster => "caster",
+            Self::Assassin => "assassin",
+            Self::Berserker => "berserker",
+            Self::Ruler => "ruler",
+            Self::Avenger => "avenger",
+            Self::Mooncancer => "mooncancer",
+            Self::Alterego => "alterego",
+            Self::Pretender => "pretender",
+            Self::Foreigner => "foreigner",
+        };
+        class_name.to_owned()
+    }
+}
