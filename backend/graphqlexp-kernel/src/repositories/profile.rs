de893ce8f1076c090e48aac5a7f7e1e39e21ed7e
SAWADA Tadashi <cesare@mayverse.jp>
2022-02-10 21:59:41 +0900

    Add profile repository

---
+++ backend/graphqlexp-kernel/src/repositories/profile.rs
@@ -0,0 +1,20 @@
+use anyhow::Result;
+use async_trait::async_trait;
+
+use crate::models::{
+    profile::{Profile, ProfileId, ProfileNumber},
+    servant::ServantId,
+};
+
+pub struct NewProfile {
+    pub servant_id: ServantId,
+    pub number: ProfileNumber,
+    pub text: String,
+}
+
+#[async_trait]
+pub trait ProfileRepository {
+    async fn find(&self, id: &ProfileId) -> Result<Option<Profile>>;
+    async fn list_for_servant(&self, servant_id: &ServantId) -> Result<Vec<Profile>>;
+    async fn register(&self, profile: NewProfile) -> Result<Profile>;
+}
