0ed0a123eb9cbb9c6f889c0e6f2b0ab8e70e9bcf
SAWADA Tadashi <cesare@mayverse.jp>
2022-02-18 22:23:26 +0900

    Add tests for profile position

--- backend/graphqlexp-kernel/src/models/profile/position.rs
+++ backend/graphqlexp-kernel/src/models/profile/position.rs
@@ -1,5 +1,6 @@
 use anyhow::{Result, bail};
 
+#[derive(Debug, PartialEq)]
 pub struct ProfilePosition(i32);
 
 impl ProfilePosition {
@@ -12,5 +13,38 @@ impl ProfilePosition {
 
     pub fn value(&self) -> i32 {
         self.0
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::ProfilePosition;
+
+    #[test]
+    fn creation_with_valid_values() {
+        let position_1 = ProfilePosition::create(1);
+        assert!(position_1.is_ok());
+        assert_eq!(ProfilePosition(1), position_1.unwrap());
+
+        let position_123 = ProfilePosition::create(123);
+        assert!(position_123.is_ok());
+        assert_eq!(ProfilePosition(123), position_123.unwrap());
+    }
+
+    #[test]
+    fn creation_with_zero() {
+        let position = ProfilePosition::create(0);
+        assert!(position.is_err());
+    }
+
+    #[test]
+    fn creation_with_negative_values() {
+        let position = ProfilePosition::create(-1);
+        assert!(position.is_err());
+    }
+
+    #[test]
+    fn values() {
+        assert_eq!(1, ProfilePosition(1).value());
     }
 }
