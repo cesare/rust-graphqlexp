eac362a255a9d32442ccdb694a2335a14a52e2c8
SAWADA Tadashi <cesare@mayverse.jp>
2022-03-01 22:33:21 +0900

    Unuse usecases

--- backend/graphqlexp-app/src/modules.rs
+++ backend/graphqlexp-app/src/modules.rs
@@ -4,7 +4,7 @@ use anyhow::Result;
 
 pub use graphqlexp_adapter::modules::RepositoriesModuleConfig;
 use graphqlexp_adapter::modules::{RepositoriesModule};
-use crate::usecase::{ListServants, ProfileRegistration, RegisterServant, ShowServant};
+use crate::usecase::{ProfileRegistration, RegisterServant};
 
 #[derive(Clone)]
 pub struct UsecasesModule {
@@ -15,14 +15,6 @@ impl UsecasesModule {
     pub async fn create(config: &dyn RepositoriesModuleConfig) -> Result<Self> {
         let repositories = RepositoriesModule::create(config).await?;
         Ok(Self { repositories: Arc::new(repositories) })
-    }
-
-    pub fn show_servant_usecase(&self) -> ShowServant {
-        ShowServant::new(&self.repositories)
-    }
-
-    pub fn list_servants_usecase(&self) -> ListServants {
-        ListServants::new(&self.repositories)
     }
 
     pub fn register_servant_usecase(&self) -> RegisterServant {
