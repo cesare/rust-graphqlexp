5a621bf1da678c5a6ed956291282c15e629cc46e
SAWADA Tadashi <cesare@mayverse.jp>
2022-03-01 21:40:32 +0900

    Fix module structures and imports

--- backend/graphqlexp-app/src/lib.rs
+++ backend/graphqlexp-app/src/lib.rs
@@ -1,3 +1,4 @@
 pub mod usecase;
 pub mod models;
 pub mod modules;
+pub mod repositories;
--- backend/graphqlexp-app/src/repositories.rs
+++ backend/graphqlexp-app/src/repositories.rs
@@ -1,7 +1,7 @@
 pub mod profile {
-    pub use graphqlexp_adapter::profile::ProfileRepository;
+    pub use graphqlexp_adapter::repositories::profile::ProfileRepository;
 }
 
 pub mod servant {
-    pub use graphqlexp_adapter::servant::ServantRepository;
+    pub use graphqlexp_adapter::repositories::servant::ServantRepository;
 }
