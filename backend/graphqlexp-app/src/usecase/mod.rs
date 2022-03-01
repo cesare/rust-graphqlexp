f0547610fc86ffccf3541cfb35a9bcea845ef4d5
SAWADA Tadashi <cesare@mayverse.jp>
2022-03-01 22:36:29 +0900

    Remove usecases

--- backend/graphqlexp-app/src/usecase/mod.rs
+++ backend/graphqlexp-app/src/usecase/mod.rs
@@ -1,11 +1,7 @@
 mod profile;
-mod list_servants;
 mod register_servant;
-mod show_servant;
 
 pub use profile::{
     registration::{ProfileAttributes, ProfileRegistration},
 };
-pub use list_servants::ListServants;
 pub use register_servant::{RegisterServant, ServantRegistration};
-pub use show_servant::ShowServant;
