0d2c78b8759949f112aac75301ef42c2de8e955c
SAWADA Tadashi <cesare@mayverse.jp>
2022-03-10 22:21:08 +0900

    Enable to generate servant -> profiles map

--- backend/graphqlexp-server/src/loaders/profile.rs
+++ backend/graphqlexp-server/src/loaders/profile.rs
@@ -15,11 +15,28 @@ impl ServantProfilesLoader {
     pub async fn load_profiles(&self, _ids: &[ServantId]) -> Result<Vec<Profile>> {
         todo!()
     }
+
+    fn create_servant_profiles(&self, profiles: Vec<Profile>) -> HashMap<ServantId, Vec<Profile>> {
+        let mut map: HashMap<ServantId, Vec<Profile>> = HashMap::new();
+        for profile in profiles {
+            let servant_id = profile.servant_id.clone();
+
+            match map.get_mut(&servant_id) {
+                Some(ps) => ps.push(profile),
+                None => {
+                    let ps: Vec<Profile> = vec![profile];
+                    map.insert(servant_id, ps);
+                }
+            }
+        }
+        map
+    }
 }
 
 #[async_trait]
 impl BatchFn<ServantId, Vec<Profile>> for ServantProfilesLoader {
-    async fn load(&mut self, _keys: &[ServantId]) -> HashMap<ServantId, Vec<Profile>> {
-        todo!()
+    async fn load(&mut self, keys: &[ServantId]) -> HashMap<ServantId, Vec<Profile>> {
+        let profiles = self.load_profiles(keys).await.unwrap();
+        self.create_servant_profiles(profiles)
     }
 }
