5c7379c0ba57c3c992909eb7bac8ace0bc2203f3
SAWADA Tadashi <cesare@mayverse.jp>
2022-03-30 23:02:19 +0900

    Re-create migrations

--- database/prisma/migrations/20220330135944_create_servants_and_profiles/migration.sql
+++ database/prisma/migrations/20220330135944_create_servants_and_profiles/migration.sql
@@ -1,7 +1,19 @@
 -- CreateTable
+CREATE TABLE "servants" (
+    "id" VARCHAR(64) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "class_name" VARCHAR(64) NOT NULL,
+    "rarity" INTEGER NOT NULL,
+    "created_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMPTZ(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "servants_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
 CREATE TABLE "profiles" (
     "id" VARCHAR(64) NOT NULL,
-    "servant_id" INTEGER NOT NULL,
+    "servant_id" VARCHAR(64) NOT NULL,
     "position" INTEGER NOT NULL,
     "text" TEXT NOT NULL,
 
