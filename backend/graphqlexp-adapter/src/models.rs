17be1b1588ed900f98acc27f9cf663b010c52b12
SAWADA Tadashi <cesare@mayverse.jp>
2022-04-01 23:09:32 +0900

    Export Id and traits

--- backend/graphqlexp-adapter/src/models.rs
+++ backend/graphqlexp-adapter/src/models.rs
@@ -1,3 +1,7 @@
+pub use graphqlexp_kernel::models::id::{
+    BelongsTo, Id, Identifiable,
+};
+
 pub mod profile {
     pub use graphqlexp_kernel::models::profile::{
         Profile, ProfileId, ProfilePosition,
--- backend/graphqlexp-app/src/models.rs
+++ backend/graphqlexp-app/src/models.rs
@@ -1,3 +1,7 @@
+pub use graphqlexp_adapter::models::{
+    BelongsTo, Id, Identifiable,
+};
+
 pub mod profile {
     pub use graphqlexp_adapter::models::profile::{
         Profile, ProfileId, ProfilePosition,
