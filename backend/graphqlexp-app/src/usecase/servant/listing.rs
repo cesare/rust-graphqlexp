9597b08a649d39611bb0334c032aab1539800939
SAWADA Tadashi <cesare@mayverse.jp>
2022-06-05 21:29:17 +0900

    Implement listing servants usecase

--- backend/graphqlexp-app/src/usecase/servant/listing.rs
+++ backend/graphqlexp-app/src/usecase/servant/listing.rs
@@ -1,14 +1,27 @@
-use graphqlexp_adapter::models::servant::Servant;
+use std::sync::Arc;
+
+use graphqlexp_adapter::{
+    models::servant::Servant,
+    modules::RepositoriesModule,
+    repositories::servant::ServantRepository,
+};
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 pub struct ListingServants {
+    repositories: Arc<RepositoriesModule>,
 }
 
 impl ListingServants {
-    pub fn new() -> Self {
-        Self {}
+    pub fn new(repositories: &Arc<RepositoriesModule>) -> Self {
+        Self {
+            repositories: repositories.clone(),
+        }
     }
 
-    pub async fn execute(&self) -> Result<Vec<Servant>, Box<dyn std::error::Error>> {
-        todo!()
+    pub async fn execute(&self) -> Result<Vec<Servant>> {
+        let repository = self.repositories.servant_repository();
+        let servants = repository.list().await?;
+        Ok(servants)
     }
 }
