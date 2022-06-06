3ad0f966096c12fa689a6fa9ec3328eb91cbf77e
SAWADA Tadashi <cesare@mayverse.jp>
2022-06-06 23:01:08 +0900

    Un-export servant repository

--- backend/graphqlexp-app/src/repositories.rs
+++ backend/graphqlexp-app/src/repositories.rs
@@ -3,7 +3,3 @@ pub use graphqlexp_adapter::repositories::Repository;
 pub mod profile {
     pub use graphqlexp_adapter::repositories::profile::ProfileRepository;
 }
-
-pub mod servant {
-    pub use graphqlexp_adapter::repositories::servant::ServantRepository;
-}
